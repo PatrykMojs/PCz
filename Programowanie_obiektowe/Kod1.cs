5d91c0d04f7f3ced83d485a2154fa2025d873b60
Patryk Meus <139061973+PatrykMojs@users.noreply.github.com>
2024-02-01 12:15:32 +0100

    Add files via upload

---
+++ Programowanie_obiektowe/Kod1.cs
@@ -0,0 +1,463 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lista3_Obiektówka
+{
+    interface IZarzadzaniePozycjami
+    {
+        void ZnajdzPozycjePoTytule(string tytul);
+        void ZnajdzPozycjePoId(int id);
+        void WypiszWszystkiePozycje();
+    }
+
+    interface IZarzadzanieBibliotekarzami
+    {
+        void ZnajdzBibliotekarzaPoImieniu(string imie);
+        void ZnajdzBibliotekarzaPoDacieZatrudnienia(string dataZatrudnienia);
+    }
+    class Katalog : IZarzadzaniePozycjami
+    {
+        private string _dzialTematyczny;
+        List<Pozycja> pozycje = new List<Pozycja>();
+
+        public Katalog()
+        {
+            _dzialTematyczny = "nieznany";
+        }
+
+        public Katalog(string dzialTematyczny)
+        {
+            _dzialTematyczny = dzialTematyczny;
+        }
+
+        public string dzialTematyczny
+        {
+            get { return _dzialTematyczny; }
+            set { _dzialTematyczny = value; }
+        }
+
+        public void DodajPozycje(Pozycja poz)
+        {
+            pozycje.Add(poz);
+        }
+
+        public void ZnajdzPozycjePoTytule(string tytul)
+        {
+            for (int i = 0; i < pozycje.Count; i++)
+            {
+                if (pozycje[i].tytul == tytul)
+                {
+                    pozycje[i].WypiszInfo();
+                }
+            }
+
+        }
+
+        public void ZnajdzPozycjePoId(int id)
+        {
+            for (int i = 0; i < pozycje.Count; i++)
+            {
+                if (pozycje[i].id == id)
+                {
+                    pozycje[i].WypiszInfo();
+                }
+            }
+        }
+
+        public void WypiszWszystkiePozycje()
+        {
+            for (int i = 0; i < pozycje.Count; i++)
+            {
+                pozycje[i].WypiszInfo();
+            }
+        }
+
+    }
+
+    abstract class Pozycja
+    {
+        private string _tytul;
+        private int _id;
+        private string _wydawnictwo;
+        private int _rokWydania;
+
+        public Pozycja()
+        {
+            _tytul = "nieznany";
+            _id = 0;
+            _wydawnictwo = "nieznany";
+            _rokWydania = 0;
+        }
+
+        public Pozycja(string tytul, int id, string wydawnictwo, int rokWydania)
+        {
+            _tytul = tytul;
+            _id = id;
+            _wydawnictwo = wydawnictwo;
+            _rokWydania = rokWydania;
+        }
+
+        public string tytul
+        {
+            get { return _tytul; }
+            set { _tytul = value; }
+        }
+
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string wydawnictwo
+        {
+            get { return _wydawnictwo; }
+            set { _wydawnictwo = value; }
+        }
+
+        public int rokWydania
+        {
+            get { return _rokWydania; }
+            set { _rokWydania = value; }
+        }
+
+        public abstract void WypiszInfo();
+    }
+
+    class Ksiazka : Pozycja
+    {
+        private int _liczbaStron;
+        List<Autor> autorzy = new List<Autor>();
+
+        public Ksiazka()
+        {
+            _liczbaStron = 0;
+        }
+
+        public Ksiazka(string tytul, int id, string wydawnictwo, int rokWydania, int liczbaStron) : base(tytul, id, wydawnictwo, rokWydania)
+        {
+            _liczbaStron = liczbaStron;
+        }
+
+        public void DodajAutora(Autor au)
+        {
+            autorzy.Add(au);
+        }
+
+        public override void WypiszInfo()
+        {
+            Console.WriteLine("Tytuł: " + tytul);
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Wydawnictwo: " + wydawnictwo);
+            Console.WriteLine("Rok wydania: " + rokWydania);
+            Console.WriteLine("Liczba stron: " + _liczbaStron);
+            for (int i = 0; i < autorzy.Count; i++)
+            {
+                Console.WriteLine("Autor: ");
+                Console.WriteLine("Imie: " + autorzy[i].imie);
+                Console.WriteLine("Nazwisko: " + autorzy[i].nazwisko);
+                Console.WriteLine("---------------------------------");
+            }
+        }
+    }
+
+    class Autor : Osoba
+    {
+        private string _narodowosc;
+
+        public Autor()
+        {
+            _narodowosc = "nieznane";
+        }
+
+        public Autor(string imie, string nazwisko, string narodowsc) : base(imie, nazwisko)
+        {
+            _narodowosc = narodowsc;
+        }
+
+        public string narodowosc
+        {
+            get { return _narodowosc; }
+            set { _narodowosc = value; }
+        }
+
+    }
+
+    class Czasopismo : Pozycja
+    {
+        private int _numer;
+
+        public Czasopismo()
+        {
+            _numer = 0;
+        }
+
+        public Czasopismo(string tytul, int id, string wydawnictwo, int rokWydania, int numer) : base(tytul, id, wydawnictwo, rokWydania)
+        {
+            _numer = numer;
+        }
+
+        public int numer
+        {
+            get { return _numer; }
+            set { _numer = value; }
+        }
+
+        public override void WypiszInfo()
+        {
+            Console.WriteLine("Tytuł: " + tytul);
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Wydawnictwo: " + wydawnictwo);
+            Console.WriteLine("Rok wydania: " + rokWydania);
+            Console.WriteLine("Numer: " + _numer);
+            Console.WriteLine("");
+        }
+    }
+
+    class Osoba
+    {
+        private string _imie;
+        private string _nazwisko;
+
+        public Osoba()
+        {
+            _imie = "nieznane";
+            _nazwisko = "nieznane";
+        }
+
+        public Osoba(string imie, string nazwisko)
+        {
+            _imie = imie;
+            _nazwisko = nazwisko;
+        }
+
+        public string imie
+        {
+            get { return _imie; }
+            set { _imie = value; }
+        }
+
+        public string nazwisko
+        {
+            get { return _nazwisko; }
+            set { _nazwisko = value; }
+        }
+    }
+
+    class Bibliotekarz : Osoba
+    {
+        private string _dataZatrudnienia;
+        private double _wynagrodzenie;
+
+        public Bibliotekarz()
+        {
+            _dataZatrudnienia = "nieznana";
+            _wynagrodzenie = 0;
+        }
+
+        public Bibliotekarz(string imie, string nazwisko, string dataZatrudnienia, double wynagrodzenie) : base(imie, nazwisko)
+        {
+            _dataZatrudnienia = dataZatrudnienia;
+            _wynagrodzenie = wynagrodzenie;
+        }
+
+        public string dataZatrudnienia
+        {
+            get { return _dataZatrudnienia; }
+            set { _dataZatrudnienia = value; }
+        }
+
+        public double wynagrodzenie
+        {
+            get { return _wynagrodzenie; }
+            set { _wynagrodzenie = value; }
+        }
+    }
+
+    class Biblioteka : IZarzadzaniePozycjami, IZarzadzanieBibliotekarzami
+    {
+        private string _adres;
+        List<Bibliotekarz> osoba = new List<Bibliotekarz>();
+        List<Katalog> katalog = new List<Katalog>();
+
+        public Biblioteka()
+        {
+            _adres = "nieznany";
+        }
+
+        public Biblioteka(string adres)
+        {
+            _adres = adres;
+        }
+
+        public string adres
+        {
+            get { return _adres; }
+            set { _adres = value; }
+        }
+
+        public void DodajBibliotekarza(Bibliotekarz os)
+        {
+            osoba.Add(os);
+        }
+
+        public void WypiszBibliotekarzy()
+        {
+            for (int i = 0; i < osoba.Count; i++)
+            {
+                Console.WriteLine("Imie: " + osoba[i].imie);
+                Console.WriteLine("Nazwisko: " + osoba[i].nazwisko);
+                Console.WriteLine("Data zatrudnienia: " + osoba[i].dataZatrudnienia);
+                Console.WriteLine("Wynagrodzenie: " + osoba[i].wynagrodzenie);
+                Console.WriteLine("");
+            }
+
+        }
+
+        public void DodajKatalog(Katalog kat)
+        {
+            katalog.Add(kat);
+        }
+
+        public void DodajPozycje(Pozycja p, string dzialTematyczny)
+        {
+            for (int i = 0; i < katalog.Count; i++)
+            {
+                if (katalog[i].dzialTematyczny == dzialTematyczny)
+                {
+                    katalog[i].DodajPozycje(p);
+                }
+            }
+        }
+
+        public void ZnajdzPozycjePoTytule(string tytul)
+        {
+            for (int i = 0; i < katalog.Count; i++)
+            {
+                katalog[i].ZnajdzPozycjePoTytule(tytul);
+            }
+        }
+
+        public void ZnajdzPozycjePoId(int id)
+        {
+            for (int i = 0; i < katalog.Count; i++)
+            {
+                katalog[i].ZnajdzPozycjePoId(id);
+            }
+        }
+
+        public void WypiszWszystkiePozycje()
+        {
+            for (int i = 0; i < katalog.Count; i++)
+            {
+                katalog[i].WypiszWszystkiePozycje();
+            }
+        }
+
+        public void ZnajdzBibliotekarzaPoImieniu(string imie)
+        {
+            for (int i = 0; i < osoba.Count; i++)
+            {
+                if (osoba[i].imie == imie)
+                {
+                    Console.WriteLine("Imie: " + osoba[i].imie);
+                    Console.WriteLine("Nazwisko : " + osoba[i].nazwisko);
+                    Console.WriteLine("Data zatrudnienia: " + osoba[i].dataZatrudnienia);
+                    Console.WriteLine("Wynagrodzenie: " + osoba[i].wynagrodzenie);
+                }
+            }
+        }
+
+        public void ZnajdzBibliotekarzaPoDacieZatrudnienia(string dataZatrudnienia)
+        {
+            for (int i = 0; i < osoba.Count; i++)
+            {
+                if (osoba[i].dataZatrudnienia == dataZatrudnienia)
+                {
+                    Console.WriteLine("Imie: " + osoba[i].imie);
+                    Console.WriteLine("Nazwisko : " + osoba[i].nazwisko);
+                    Console.WriteLine("Data zatrudnienia: " + osoba[i].dataZatrudnienia);
+                    Console.WriteLine("Wynagrodzenie: " + osoba[i].wynagrodzenie);
+                }
+            }
+        }
+
+        public void ZnajdzBibliotekarzaPoWynagrodzeniu(double wynagrodzenie)
+        {
+            for (int i = 0; i < osoba.Count; i++)
+            {
+                if (osoba[i].wynagrodzenie == wynagrodzenie)
+                {
+                    Console.WriteLine("Imie: " + osoba[i].imie);
+                    Console.WriteLine("Nazwisko : " + osoba[i].nazwisko);
+                    Console.WriteLine("Data zatrudnienia: " + osoba[i].dataZatrudnienia);
+                    Console.WriteLine("Wynagrodzenie: " + osoba[i].wynagrodzenie);
+                }
+            }
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zadanie 1
+            Console.WriteLine("Zadanie 1");
+            Pozycja ksiazka = new Ksiazka("Super tytul", 1, "Wydawnictwo Ksiazek", 2021, 200);
+            Pozycja czasopismo = new Czasopismo("Czasopismo jakies", 1, "Wydawnictwo Czasopism", 2022, 20);
+            Katalog katalog = new Katalog("Katalog_nazwa");
+            katalog.DodajPozycje(ksiazka);
+            Console.WriteLine(katalog.dzialTematyczny);
+            ksiazka.WypiszInfo();
+            czasopismo.WypiszInfo();
+         
+
+            //zadanie 2
+            Console.WriteLine("Zadanie 2");
+            Biblioteka biblioteka = new Biblioteka("Nazwa Bibliteki");
+            Bibliotekarz b = new Bibliotekarz("Anna", "Kowalska", "05.04.2000", 3000); ;
+            Bibliotekarz b2 = new Bibliotekarz("Jan", "Nowak", "02.05.1985", 2500);
+            biblioteka.DodajBibliotekarza(b);
+            biblioteka.DodajBibliotekarza(b2);
+            biblioteka.WypiszBibliotekarzy();
+            
+
+            Katalog katalog1 = new Katalog("fantasy");
+
+            biblioteka.DodajKatalog(katalog1);
+
+            Autor autor1 = new Autor("Dawid", "Robak", "Polska");
+            Autor autor2 = new Autor("George", "Johnson", "Anglia");
+
+            Ksiazka ksiazka1 = new Ksiazka("Tutul Dawida", 2, "dwd", 2012, 4221);
+
+            Pozycja pozycja = ksiazka1;
+            Ksiazka ksiazka2 = new Ksiazka("Tutul Georga", 3, "grg", 1999, 5231);
+            Pozycja pozycja2 = new Czasopismo("Super", 4, "hwh", 2002, 50);
+
+            ksiazka1.DodajAutora(autor1);
+            ksiazka2.DodajAutora(autor2);
+
+            pozycja.WypiszInfo();
+            ksiazka1.DodajAutora(autor2);
+            pozycja.WypiszInfo();
+
+            katalog1.DodajPozycje(pozycja);
+            katalog1.WypiszWszystkiePozycje();
+
+            biblioteka.DodajPozycje(pozycja2, "pozycja");
+            biblioteka.ZnajdzPozycjePoTytule("Tutul Georga");
+            biblioteka.ZnajdzPozycjePoId(2);
+            biblioteka.WypiszWszystkiePozycje();
+            Console.WriteLine("\n");
+
+            biblioteka.ZnajdzBibliotekarzaPoImieniu("Jan");
+            biblioteka.ZnajdzBibliotekarzaPoDacieZatrudnienia("05.04.2000");
+            biblioteka.ZnajdzBibliotekarzaPoWynagrodzeniu(3000);
+
+            Console.WriteLine("Aplikacja konsolowa zakończona. Naciśnij Enter, aby zamknąć konsolę.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Programowanie_obiektowe/Kod2.cs
@@ -0,0 +1,441 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    class Zwierze
+    {
+        private string pozywienie;
+        private int wiek;
+        private string pochodzenie;
+
+        public string Pozywienie
+        {
+            get { return pozywienie; }
+            set { pozywienie = value; }
+        }
+        public int Wiek
+        {
+            get { return wiek; }
+            set { wiek = value; }
+        }
+        public string Pochodzenie
+        {
+            get { return pochodzenie; }
+            set { pochodzenie = value; }
+        }
+
+        public Zwierze()
+        {
+            this.pozywienie = " ";
+            this.wiek = 0;
+            this.pochodzenie = " ";
+        }
+        public Zwierze(string pozywienie, int wiek, string pochodzenie)
+        {
+            this.pozywienie = pozywienie;
+            this.wiek = wiek;
+            this.pochodzenie = pochodzenie;
+        }
+
+        public virtual void WypiszInfo()
+        {
+            Console.WriteLine($"Pożywienie: {pozywienie}\nWiek: {wiek}\nPochodzenie: {pochodzenie}");
+        }
+    }
+
+    class Ssak : Zwierze
+    {
+        private string srodowisko;
+
+        public string Srodowisko
+        {
+            get { return srodowisko; }
+            set { srodowisko = value; }
+        }
+
+        public Ssak() : base()
+        {
+            this.srodowisko = " ";
+        }
+
+        public Ssak(string pozywienie, int wiek, string pochodzenie, string srodowisko) :
+            base(pozywienie, wiek, pochodzenie)
+        {
+            this.srodowisko = srodowisko;
+        }
+
+        // Dziedziczy z klasy zwierze WypiszInfo()
+        // this.GetType().name to nazwa klasy, żeby było wiadomo jaki gatunek
+        public override void WypiszInfo()
+        {
+            Console.WriteLine($"\n{this.GetType().Name}");
+            base.WypiszInfo();
+            Console.WriteLine($"Naturalne środowisko: {srodowisko}");
+        }
+    }
+
+    class Gad : Zwierze
+    {
+        private bool trujacy;
+
+        public bool Trujacy
+        {
+            get { return trujacy; }
+            set { trujacy = value; }
+        }
+
+        public Gad() : base()
+        {
+            this.trujacy = false;
+        }
+
+        public Gad(string pozywienie, int wiek, string pochodzenie, bool trujacy) :
+            base(pozywienie, wiek, pochodzenie)
+        {
+            this.trujacy = trujacy;
+        }
+
+        // Dziedziczy z klasy zwierze WypiszInfo()
+        // this.GetType().name to nazwa klasy, żeby było wiadomo jaki gatunek
+        public override void WypiszInfo()
+        {
+            Console.WriteLine($"\n{this.GetType().Name}");
+            base.WypiszInfo();
+            Console.WriteLine($"Trujący: {trujacy}");
+        }
+    }
+
+    class Ptak : Zwierze
+    {
+        private double rozpietoscSkrzydel;
+        private int wytrzymalosc;
+
+        public double RozpietoscSkrzydel
+        {
+            get { return rozpietoscSkrzydel; }
+            set { rozpietoscSkrzydel = value; }
+        }
+        public int Wytrzymalosc
+        {
+            get { return wytrzymalosc; }
+            set { wytrzymalosc = value; }
+        }
+
+        public Ptak() : base()
+        {
+            this.rozpietoscSkrzydel = 0.0;
+            this.wytrzymalosc = 0;
+        }
+
+        public Ptak(string pozywienie, int wiek, string pochodzenie, double rozpietoscSkrzydel, int wytrzymalosc) :
+            base(pozywienie, wiek, pochodzenie)
+        {
+            this.rozpietoscSkrzydel = 0.0;
+            this.wytrzymalosc = 0;
+        }
+
+        // Dziedziczy z klasy zwierze WypiszInfo()
+        // this.GetType().name to nazwa klasy, żeby było wiadomo jaki gatunek
+        public override void WypiszInfo()
+        {
+            Console.WriteLine($"\n{this.GetType().Name}");
+            base.WypiszInfo();
+            Console.WriteLine($"Rozpiętość skrzydeł: {rozpietoscSkrzydel}\nWytrzymałość: {wytrzymalosc}");
+        }
+    }
+
+    class Opiekun
+    {
+        private List<Klatka> klatki = new List<Klatka>();
+
+        private string imie;
+        private string nazwisko;
+        private string dataUrodzenia;
+        private string dataZatrudnienia;
+
+        // Gettery i settery
+        public string Imie
+        {
+            get { return imie; }
+            set { imie = value; }
+        }
+        public string Nazwisko
+        {
+            get { return nazwisko; }
+            set { nazwisko = value; }
+        }
+        public string DataUrodzenia
+        {
+            get { return dataUrodzenia; }
+            set { dataUrodzenia = value; }
+        }
+        public string DataZatrudnienia
+        {
+            get { return dataZatrudnienia; }
+            set { dataZatrudnienia = value; }
+        }
+
+        public Opiekun()
+        {
+            this.imie = " ";
+            this.nazwisko = " ";
+            this.dataUrodzenia = " ";
+            this.dataZatrudnienia = " ";
+        }
+
+        public Opiekun(string imie, string nazwisko, string dataUrodzenia, string dataZatrudnienia)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.dataUrodzenia = dataUrodzenia;
+            this.dataZatrudnienia = dataZatrudnienia;
+        }
+
+        // Przypisujemy opiekunowi klatkę
+        public void PrzypiszKlatke(Klatka k)
+        {
+            klatki.Add(k);
+        }
+
+        // Podajemy klatkę do wyczyszczenia
+        public void WyczyscKlatke(Klatka k)
+        {
+            k.Czysta = true;
+        }
+
+
+        // Wypisuje info o osobach oraz jakimi klatkami się zajmują
+        public void WypiszInfo()
+        {
+            Console.WriteLine($"Imie: {imie}\nNazwisko: {nazwisko}\n" +
+                $"Data urodzenia: {dataUrodzenia}\nData zatrudnienia: {dataZatrudnienia}");
+            Console.WriteLine("Klatki należące do tej osoby:");
+            foreach (Klatka k in klatki)
+            {
+                Console.WriteLine($"ID klatki: {k.Id}");
+            }
+        }
+    }
+
+    class Klatka
+    {
+        private List<Zwierze> zwierzeta = new List<Zwierze>();
+
+        private int pojemnosc;
+        private int id;
+        private bool czysta;
+
+        public int Pojemnosc
+        {
+            get { return pojemnosc; }
+            set { pojemnosc = value; }
+        }
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public bool Czysta
+        {
+            get { return czysta; }
+            set { czysta = value; }
+        }
+
+        public Klatka()
+        {
+            this.pojemnosc = 0;
+            this.id = 0;
+            this.czysta = true;
+        }
+
+        public Klatka(int pojemnosc, int id, bool czysta)
+        {
+            this.pojemnosc = pojemnosc;
+            this.id = id;
+            this.czysta = czysta;
+        }
+
+        // Sprawdza czy jest miejsce w klatce, jeżeli nie ma to daje kominukat
+        // w przeciwnym razie dodaje zwierze
+        public void DodajZwierze(Zwierze z)
+        {
+            if (zwierzeta.Count >= pojemnosc)
+            {
+                Console.WriteLine("Klatka jest pełna");
+            }
+            else
+            {
+                zwierzeta.Add(z);
+            }
+        }
+
+        // Wypisuje informacje o klatce oraz zwierzętach w tej klatce
+        public void WypiszInfo()
+        {
+            Console.WriteLine($"\nID: {id}\nPojemność: {pojemnosc}\nCzysta?: {czysta}");
+            Console.WriteLine("Zwierzęta w tej klatce:");
+            for (int i = 0; i < zwierzeta.Count; i++)
+            {
+                zwierzeta[i].WypiszInfo();
+            }
+        }
+    }
+
+    class Zoo
+    {
+        private List<Klatka> klatki = new List<Klatka>();
+        public List<Opiekun> pracownicy = new List<Opiekun>();
+
+        private string nazwa;
+
+        public string Nazwa
+        {
+            get { return nazwa; }
+            set { nazwa = value; }
+        }
+
+        public Zoo()
+        {
+            this.nazwa = " ";
+        }
+
+        public Zoo(string nazwa)
+        {
+            this.nazwa = nazwa;
+        }
+
+        public void DodajKlatke(Klatka k)
+        {
+            klatki.Add(k);
+        }
+
+        public void DodajKlatke(int pojemnosc, int id, bool czysta)
+        {
+            Klatka k = new Klatka(pojemnosc, id, czysta);
+            klatki.Add(k);
+        }
+
+        public void ZatrudnijPracownika(Opiekun o)
+        {
+            pracownicy.Add(o);
+        }
+
+        // Poszukuje podanej klatki i zwiększa jej rozmiar
+        public void ZwiekszRozmiarKlatki(Klatka k, int rozmiar)
+        {
+            for (int i = 0; i < klatki.Count; i++)
+            {
+                if (klatki[i] == k)
+                {
+                    klatki[i].Pojemnosc += rozmiar;
+                }
+            }
+        }
+
+        // Poszukuje klatki po podanym id i zwiększa jej rozmiar
+        public void ZwiekszRozmiarKlatki(int id, int rozmiar)
+        {
+            for (int i = 0; i < klatki.Count; i++)
+            {
+                if (klatki[i].Id == id)
+                {
+                    klatki[i].Pojemnosc += rozmiar;
+                }
+            }
+        }
+
+        // Wypisuje info o klatkach oraz o zwierzętach w nich
+        public void WypiszInfoKlatki()
+        {
+            foreach (Klatka k in klatki)
+            {
+                k.WypiszInfo();
+                Console.WriteLine("------------");
+            }
+        }
+
+        // Wypisuje informacje o pracownikach
+        public void WypiszInfoPracownicy()
+        {
+            foreach (Opiekun o in pracownicy)
+            {
+                o.WypiszInfo();
+                Console.WriteLine("-------------------");
+            }
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zoo zoo = new Zoo("Zoo Wrocław");
+
+            Zwierze pies = new Ssak("Wszystko", 4, "Cały świat", "Cały świat");
+            Zwierze kot = new Ssak("Wszystko", 3, "Cały świat", "Cały świat");
+            Zwierze krowa = new Ssak("Wszystko", 6, "Europa", "Europa");
+            Zwierze bocian = new Ptak("Wszystko", 2, "Europa", 124.5, 10);
+            Zwierze kobra = new Gad("Wszystko", 6, "Afryka", true);
+            Zwierze krokodyl = new Gad("Mięso", 1, "Afryka", true);
+
+            Klatka klatka1 = new Klatka(2, 123, true);
+            Klatka klatka2 = new Klatka(3, 456, true);
+            Klatka klatka3 = new Klatka(5, 789, true);
+
+            klatka1.DodajZwierze(pies);
+            klatka1.DodajZwierze(kot);
+            klatka1.DodajZwierze(krowa);
+            zoo.DodajKlatke(klatka1);
+            zoo.DodajKlatke(klatka2);
+            zoo.DodajKlatke(klatka3);
+
+            klatka2.DodajZwierze(bocian);
+            klatka3.DodajZwierze(kobra);
+            klatka3.DodajZwierze(krokodyl);
+
+            zoo.ZwiekszRozmiarKlatki(123, 2);
+
+            klatka1.DodajZwierze(krowa);
+
+            Opiekun o1 = new Opiekun("Jan", "Kowalski", "12.03.1998", "05.04.2020");
+            Opiekun o2 = new Opiekun("Anna", "Nowak", "23.07.2000", "08.05.2021");
+
+            zoo.ZatrudnijPracownika(o1);
+            zoo.ZatrudnijPracownika(o2);
+
+            o1.PrzypiszKlatke(klatka1);
+            o2.PrzypiszKlatke(klatka2);
+            o2.PrzypiszKlatke(klatka3);
+
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("Informaje o klatkach");
+            zoo.WypiszInfoKlatki();
+            Console.WriteLine("------------------------------------------");
+
+            klatka2.Czysta = false;
+
+            klatka2.WypiszInfo();
+            Console.WriteLine("------------------------------------------");
+
+            o2.WyczyscKlatke(klatka2);
+
+            Console.WriteLine("------------------------------------------");
+            klatka2.WypiszInfo();
+            Console.WriteLine("------------------------------------------");
+
+            zoo.WypiszInfoPracownicy();
+
+
+
+            Console.ReadKey();
+        }
+    }
+
+
+
+}
